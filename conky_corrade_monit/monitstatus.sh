087e563076013f6ddf8bc22ba590fce0e968513d
freddii <https://freddii@github.com>
2019-10-24 11:05:49 +0200

    update corrade monit

--- conky_corrade_monit/monitstatus.sh
+++ conky_corrade_monit/monitstatus.sh
@@ -5,9 +5,13 @@ source $pathConky/corrade_setup
 
 rawdatadecode() {
 #QUERY_STRING='param1=value1&param2=value2&param3=value3'
-QUERY_STRING=$(curl -d "command=execute&group=$GroupUUID&password=$GroupPassword&file=/usr/bin/sudo&parameter= /usr/bin/monit status corrade$BotFolderName" $HttpServer)
+QUERY_STRING=$(curl -d "command=execute&group=$GroupUUID&password=$GroupPassword&file=/usr/bin/sudo&parameter= /usr/bin/monit status" $HttpServer) # corrade$BotFolderName
 arr=(${QUERY_STRING//[=&]/ })
 echo ${arr[5]} | sed -e 's/%20/ /g' | sed -e $'s/%0A/\\\n/g' | sed -e 's/%25/%/g' | sed -e 's/%5B/[/g' | sed -e 's/%5D/]/g' | sed -e 's/%3A/:/g' | sed -e 's|%2F|/|g' | sed -e "s|%27|'|g" | sed -e 's|%2C|,|g' | sed -e 's/data collected               .*, /data collected               /g'
 }
 
+if [ "$(iwgetid -r)" == "$HomeWifiSSID" ]; then 
 rawdatadecode
+else
+echo "$(iwgetid -r)"" is not home wifi."
+fi
