6529d9335d819df7a920cc8e525d1fc107ba1d66
freddii <https://freddii@github.com>
2020-11-17 03:30:55 +0100

    update

--- conky_corrade/version.sh
+++ conky_corrade/version.sh
@@ -3,11 +3,18 @@
 pathConky="$( cd "$(dirname "$0")" ; pwd -P )"
 source $pathConky/corrade_setup
 
+urldecode() {
+    # urldecode <string>
+
+    local url_encoded="${1//+/ }"
+    printf '%b' "${url_encoded//%/\\x}"
+}
+
 rawdatadecode() {
 #QUERY_STRING='param1=value1&param2=value2&param3=value3'
 QUERY_STRING=$(curl -d "command=version&group=$GroupUUID&password=$GroupPassword" $HttpServer)
 arr=(${QUERY_STRING//[=&]/ })
-echo "corrade v"${arr[5]}
+echo "corrade v"$(urldecode ${arr[5]})  #sudo apt install renameutils
 }
 
 if [ "$(iwgetid -r)" == "$HomeWifiSSID" ]; then 
